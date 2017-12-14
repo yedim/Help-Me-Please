8ef740cc2afd2389d6a79d464efe0d8adff0cc95
yedim <yelimi0416@gmail.com>
2017-12-14 17:53:25 +0900

    Pause Button

---
+++ Help Me Please/Assets/PauseMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public GameObject pauseMenu;
+ 
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        pauseMenu.SetActive(true);
+    }
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+    }
+
+    public void QuitToMain()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
+
