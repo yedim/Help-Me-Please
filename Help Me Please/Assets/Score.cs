e596670df2f3b681691ebe8bc50b7c31c1984c80
yedim <yelimi0416@gmail.com>
2017-12-14 17:56:06 +0900

    Score

---
+++ Help Me Please/Assets/Score.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    public static int currentScore;
+    public Text scoreText;
+    public Text resultScoreText;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentScore = 0;
+        resultScoreText.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = ("" + currentScore);
+        resultScoreText.text = scoreText.text;
+    }
+    public static void addScore()
+    {
+        currentScore += 10;
+    }
+}
